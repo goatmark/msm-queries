1258f5c4b803ed9f38b811f7acadb527c13cbb8f
goatmark <102983476+goatmark@users.noreply.github.com>
2024-11-02 03:32:51 +0000

    intiial working

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -4,11 +4,24 @@ class DirectorController < ApplicationController
   end
 
   def eldest
+    eldest_director = Director.where.not({:dob => nil}).order({ :dob => :desc }).at(0)
+
+    # Director Name
+    @eldest_director_name = eldest_director.name
+    dob_string = eldest_director.dob
+    @eldest_director_dob = dob_string.strftime("%B %m, %Y")
     
     render({ :template => "director_templates/eldest"})
   end
 
   def youngest
+    youngest_director = Director.where.not({:dob => nil}).order({ :dob => :asc }).at(0)
+
+    # Director Name
+    @youngest_director_name = youngest_director.name
+    dob_string = youngest_director.dob
+    @youngest_director_dob = dob_string.strftime("%B %m, %Y")
+
     render({ :template => "director_templates/youngest"})
   end
 end
